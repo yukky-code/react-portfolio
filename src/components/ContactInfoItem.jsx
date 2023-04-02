c852df73bbffe5152b175bbc4ff45b6f7fc17b44
yukky-code <yukky.tomo.0331@gmail.com>
2023-04-02 15:55:40 +0900

    ✨ 修正

--- src/components/ContactInfoItem.jsx
+++ src/components/ContactInfoItem.jsx
@@ -4,30 +4,37 @@ import PText from './PText'
 import { MdAddIcCall } from "react-icons/md";
 
 const ItemStyles = styled.div`
-  padding: 2rem;
+  padding: 2rem 1rem;
   background-color: #212121;
   display: flex;
   align-items: center;
-  gap: 2rem;
+  gap: 0.5rem;
   border-radius: 8px;
   margin-bottom: 2rem;
   color: white;
   .icon {
     color: white;
     background-color: #2B2B2B;
-    padding: 1.3rem;
+    padding: 1rem;
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 50%;
   }
+
+  @media only screen and (max-width: 768px) {
+    .icon {
+        padding: 0.3rem;
+      }
+  }
+
   svg {
     width: 1.5rem;
   }
   .info p {
     margin-top: 0;
     margin-bottom: 0;
-    font-size: 20px;
+    font-size: 1rem;
   }
 `;
 
